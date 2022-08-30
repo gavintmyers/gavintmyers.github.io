f5993f732691029833711711853371574d2f8e9d
Gavin Myers <gavinm@pvep.com>
2022-08-30 19:48:48 -0400

    First rough draft with a portfolio item and moon

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+import logo from './moon.png';
 import './App.css';
+import Portfolio from './Portfolio';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={logo} className="Moon-image" alt="logo" />
+        <p></p>
+        <br/>
+        <code>Gavin Myers personal site</code>
+        <br/>
+        <code>(in progress)</code>
+        <br/>
+        <br/>
+        <br/>
       </header>
+      <body>
+        <Portfolio />
+      </body>
     </div>
   );
 }
---
+++ src/Portfolio.js
@@ -0,0 +1,28 @@
+import './Portfolio.css';
+
+function Portfolio() {
+    return (
+        <div className="Portfolio">
+            <header className="Portfolio-header">
+                <p>
+                    This is a sort of Portfolio?
+                </p>
+            </header>
+            <body>
+                <a
+                    className="App-link"
+                    href="https://github.com/annkissam/scryglass#readme"
+                    target="_blank" // opens in new tab
+                    rel="noopener noreferrer"
+                >
+                    Scryglass Ruby Tool
+                </a>
+                {' '} that i made {/* or can use &nbsp; */}
+                <br/><br/>
+                [Other projects to fill out]
+            </body>
+        </div>
+    );
+}
+
+export default Portfolio;
